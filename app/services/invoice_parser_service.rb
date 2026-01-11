c300a5e92141c598b1ad921802a25b64208b5251
Grzegorz Jasek <grzegorz.jasek@comp-ci.pl>
2026-01-11 13:34:02 +0100

    change the data collecting from azure raw data

--- app/services/invoice_parser_service.rb
+++ app/services/invoice_parser_service.rb
@@ -1,102 +1,67 @@
+# invoice parser service
 class InvoiceParserService
-
   def initialize(invoice)
-    @lines = JSON.parse(invoice.parsed_azure_invoice_data).values
+    @invoice = invoice
+    @lines = JSON.parse(invoice.parsed_azure_invoice_data).values.map(&:to_s)
   end
 
   def parse
     {
-      invoice_number: extract_invoice_number,
-      issue_date: extract_issue_date,
-      seller: extract_seller,
-      buyer: extract_buyer,
-      items: extract_items,
-      totals: extract_totals
-    }
-  end
-
-  private
-
-  def extract_invoice_number
-    line = @lines.find { |l| l.include?('Numer:') || l =~ /nr\s*\d+/i }
-    line&.sub(/Numer:\s*/, '')&.strip || line&.strip
-  end
-
-  def extract_issue_date
-    issue_line = @lines.find { |l| l =~ /Data wystawienia[:]?/i }
-    issue_date = issue_line&.sub(/Data wystawienia[:]?/, '')&.strip
-    issue_date ||= @lines.find { |l| l =~ /\d{4}-\d{2}-\d{2}/ } # YYYY-MM-DD
-    issue_date ||= @lines.find { |l| l =~ /\d{2}-\d{2}-\d{4}/ } # DD-MM-YYYY
-
-    sale_line = @lines.find { |l| l =~ /Data sprzedaży[:]?/i }
-    sale_date = sale_line&.sub(/Data sprzedaży[:]?/, '')&.strip
-    sale_date ||= @lines.find { |l| l =~ /\d{4}-\d{2}-\d{2}/ } # fallback
-
-    { issue_date: issue_date, sale_date: sale_date }
-
-  end
-
-  def extract_seller
-    start_idx = @lines.find_index { |l| l.include?('Sprzedawca:') } || 0
-    end_idx   = @lines.find_index { |l| l.include?('Nabywca:') } || (start_idx + 6)
-    seller_lines = @lines[(start_idx + 1)...end_idx]
-
-    {
-      name: seller_lines[0],
-      street: seller_lines[1],
-      branch: seller_lines[2],
-      street2: seller_lines[3],
-      bdo: seller_lines[4]&.sub('BDO ', ''),
-      nip: seller_lines[5]&.sub("NIP[:\s]*", '')&.strip
-    }
-  end
-
-  def extract_buyer
-    start_idx = @lines.find_index { |l| l.include?('Nabywca:') } || 0
-    buyer_lines = @lines[(start_idx + 1)..-1]
-
-    {
-      name: buyer_lines[0],
-      street: buyer_lines[1],
-      city: buyer_lines[2],
-      nip: buyer_lines[3]&.sub("NIP[:\s]*", '')&.strip
-    }
-  end
-
-  def extract_items
-    items = []
-
-    @lines.each_with_index do |line, idx|
-      next unless line.match?(/^[A-Z].*\d/) # heurystyka produktu
-
-      net       = @lines[idx + 1] rescue nil
-      vat_rate  = @lines[idx + 2] rescue nil
-      vat_value = @lines[idx + 3] rescue nil
-      gross     = @lines[idx + 4] rescue nil
-
-      items << {
-        name: line,
-        net: net,
-        vat_rate: vat_rate,
-        vat_value: vat_value,
-        gross: gross
+      extracted: {
+        invoice_nr: extract_near(@invoice.invoice_nr),
+        user_nip: extract_nip(@invoice.user_nip),
+        invoice_date: match_date(@invoice.invoice_date),
+        gross: match_amount(@invoice.brutto),
+        invoice_number: extract_near(@invoice.invoice_nr),
+        seller_nip: extract_near(@invoice.company&.nip),
       }
-    end
-
-    items
-  end
-
-  def extract_totals
-    sum_idx = @lines.find_index { |l| l.include?('SUMA PLN') } || -1
-    vat_idx = @lines.find_index { |l| l.include?('VAT') } || -1
-
-    {
-      net: (@lines[sum_idx - 2] rescue nil),
-      vat: (@lines[vat_idx] rescue nil),
-      gross: (@lines[sum_idx - 1] rescue nil)
     }
   end
 
+  def match_invoice_number
+    @lines.any? do |line|
+      normalize_text(line).include?(normalize_text(@invoice.invoice_nr))
+    end
+  end
 
+  def extract_nip(nip)
+    needle = nip.gsub(/\D/, '')
 
+    @lines.find do |line|
+      line.gsub(/\D/, '').include?(needle)
+    end
+  end
+
+  def match_date(date)
+    variants = [
+      date.strftime('%Y-%m-%d'),
+      date.strftime('%d-%m-%Y'),
+      date.strftime('%d.%m.%Y'),
+      date.strftime('%Y.%m.%d')
+    ]
+
+    @lines.find do |line|
+      variants.find { |v| line.include?(v) }
+    end
+  end
+
+  def match_amount(amount)
+    normalized = format('%.2f', amount.to_f).tr('.', ',')
+
+    @lines.find do |line|
+      line.include?(normalized) || line.include?(normalized.tr(',', '.'))
+    end
+  end
+
+  def extract_near(value)
+    needle = normalize_text(value)
+
+    @lines.find do |line|
+      normalize_text(line).include?(needle)
+    end
+  end
+
+  def normalize_text(text)
+    text.to_s.downcase.gsub(/[^\p{L}\p{N}]/, '')
+  end
 end
